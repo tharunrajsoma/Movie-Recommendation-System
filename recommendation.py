4a1e91b865949380db8bc2abc98798e3cbfbef8a
Tharun Raj Soma <tsoma@codeaurora.org>
2019-04-21 13:59:15 +0530

    First commit

---
+++ recommendation.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from rake_nltk import Rake
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+pd.set_option('display.max_columns', 120)
+df = pd.read_csv('data/IMDB_Top250Engmovies2_OMDB_Detailed.csv')
+
+# Select those features on which your recommendation should base on
+df = df[['Title','Genre','Director','Actors','Plot']]
+
+# I am considering only first 3 actors names by removing commas between actors' names
+df['Actors'] = df['Actors'].map(lambda x: x.split(',')[:3])
+
+# populating genres into a list of words
+df['Genre'] = df['Genre'].map(lambda x: x.lower().split(','))
+
+df['Director'] = df['Director'].map(lambda x: x.split(' '))
+
+# merging together first and last name for each actor and director,
+# With this it becomes one word and there will be no mix up between people having common first name
+for index, row in df.iterrows():
+    row['Actors'] = [x.lower().replace(' ','') for x in row['Actors']]
+    row['Director'] = ''.join(row['Director']).lower()
+
+
+# initializing the new column
+df['Key_words'] = ""
+
+for index, row in df.iterrows():
+    plot = row['Plot']
+    
+    # instantiating Rake, by default it uses english stopwords from NLTK
+    # and discards all puntuation characters as well
+    r = Rake()
+
+    # extracting the words by passing the text
+    r.extract_keywords_from_text(plot)
+
+    # getting the dictionary whith key words as keys and their scores as values
+    key_words_dict_scores = r.get_word_degrees()
+    
+    # assigning the key words to the new column for the corresponding movie
+    row['Key_words'] = list(key_words_dict_scores.keys())
+
+# dropping the Plot column
+df.drop(columns = ['Plot'], inplace = True)
+df.set_index('Title', inplace = True)
+
+
+df['words_bag'] = ''
+columns = df.columns
+for index, row in df.iterrows():
+    words = ''
+    for col in columns:
+        if col != 'Director':
+            words = words + ' '.join(row[col])+ ' '
+        else:
+            words = words + row[col]+ ' '
+    row['words_bag'] = words
+    
+df.drop(columns = [col for col in df.columns if col!= 'words_bag'], inplace = True)
+
+# instantiating and generating the count matrix
+count = CountVectorizer()
+count_matrix = count.fit_transform(df['words_bag'])
+
+
+# creating a Series for the movie titles so that they are associated to an ordered numerical list 
+# which we will use in the function to match the indexes
+indices = pd.Series(df.index)
+
+# generating the cosine similarity matrix
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+# Function that takes in movie title as input and can
+# return top 10 movies similar to this based on content 
+# using NLP and Cosine Similarity techniques
+def movie_recommendations(title, cosine_sim = cosine_sim):
+    
+    # gettin the index of the movie that matches the title
+    idx = indices[indices == title].index[0]
+
+    # creating a Series with the similarity scores in descending order
+    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+
+    # getting the indexes of the 10 most similar movies
+    top_10_indexes = list(score_series.iloc[1:11].index)
+    
+    # Printing the list of top 10 recommended movies
+    serial_no = 1
+    print('\nList of recommended movies:')
+    for i in top_10_indexes:
+        print(serial_no,'.',list(df.index)[i])
+        serial_no+=1  
+
+movie_recommendations('Interstellar')
